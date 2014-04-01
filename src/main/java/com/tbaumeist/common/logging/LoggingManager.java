9afb9d287a0f5170bc900019a01caeb01ae0a662
Todd Baumeister <tbaumeist@gmail.com>
2014-04-01 15:06:23 -0700

    Added functinality to insert a file log handler

--- src/main/java/com/tbaumeist/common/logging/LoggingManager.java
+++ src/main/java/com/tbaumeist/common/logging/LoggingManager.java
@@ -1,15 +1,29 @@
 package com.tbaumeist.common.logging;
 
+import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class LoggingManager {
     public static void initialize(){
         Logger logger = Logger.getLogger("");
         for (Handler h : logger.getHandlers()) {
             h.setFormatter(new ReallySimpleFormatter());
-            h.setLevel(Level.ALL);
+            h.setLevel(Level.SEVERE);
         }
+    }
+    
+    public static void addLogFile(String fileName, Level level) throws Exception{
+        if(fileName == null || fileName.isEmpty())
+            return;
+        
+        Logger logger = Logger.getLogger("");
+        FileHandler file = new FileHandler(fileName);
+        file.setLevel(level);
+        file.setFormatter(new SimpleFormatter());
+        logger.addHandler(file);
+        logger.info("Added file logger " + fileName);
     }
 }
